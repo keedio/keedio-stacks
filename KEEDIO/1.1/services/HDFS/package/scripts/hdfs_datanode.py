40b3ca6355a13755592e81368c8276204ad9dbee
alcuma <cuma@Alessios-MacBook-Pro.local>
2015-05-27 15:38:28 +0200

    Committer: alcuma <cuma@Alessios-MacBook-Pro.local>  On branch development  Changes to be committed: 	modified:   hdfs_datanode.py 	modified:   hdfs_snamenode.py

--- KEEDIO/1.1/services/HDFS/package/scripts/hdfs_datanode.py
+++ KEEDIO/1.1/services/HDFS/package/scripts/hdfs_datanode.py
@@ -42,6 +42,9 @@ def datanode(action=None):
   if action == "start":
     cmd=Popen(['service','hadoop-hdfs-datanode',action],stdout=PIPE,stderr=STDOUT)
     out,err=cmd.communicate()
+    Logger.info("Alessio: starting datanode")
+    Logger.info(out)
+    Logger.info(err)
     rc = cmd.returncode
     Logger.info("Datanode service %s: %s" % (action, rc == 0))
     check_rc(rc,stdout=out,stderr=err)
@@ -49,10 +52,17 @@ def datanode(action=None):
   if action == "stop":
     Logger.info("Datanode service %s")
     cmd=Popen(['service','hadoop-hdfs-datanode',action],stdout=PIPE,stderr=STDOUT)
+    out,err=cmd.communicate()
+    Logger.info("Alessio: stopping datanode")
+    Logger.info(out)
+    Logger.info(err)
 
   if action == "status":
     cmd=Popen(['service','hadoop-hdfs-datanode',action],stdout=PIPE,stderr=STDOUT)
     out,err=cmd.communicate()
+    Logger.info("Alessio:Checking datanode status")
+    Logger.info(out)
+    Logger.info(err)
     rc = cmd.returncode
     Logger.info("Datanode service %s: %s" % (action, rc == 0))
     check_rc(rc,stdout=out,stderr=err)
