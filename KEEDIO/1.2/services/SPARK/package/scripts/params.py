d62d39d093e55cf24ce4493ea17e11d6e64a680d
Juan Carlos Fernandez <jcfernandez@keedio.com>
2015-05-29 15:31:48 +0200

    Initial SPARK commit

---
+++ KEEDIO/1.2/services/SPARK/package/scripts/params.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+from resource_management.libraries.functions.version import format_hdp_stack_version, compare_versions
+from resource_management import *
+
+config = Script.get_config()
+
+security_enabled = config['configurations']['cluster-env']['security_enabled']
+
+kerberos_cache_file = config['configurations']['cluster-env']['kerberos_cache_file']
+
+spark_user = config['configurations']['spark']['spark_user']
+spark_group = config['configurations']['spark']['spark_group']
+spark_home = config['configurations']['spark']['spark_home']
+spark_principal = default('/configurations/spark/spark_principal',None)
+spark_keytab = default('/configurations/spark/spark_keytab',None)
+spark_yarn_lib_dir = config['configurations']['spark']['spark_yarn_lib_dir']
+spark_assembly = config['configurations']['spark']['spark_assembly']
+
+hdfs_user = config['configurations']['hadoop-env']['hdfs_user']
+hdfs_principal_name = default('/configurations/hadoop-env/hdfs_principal_name',None)
+hdfs_user_keytab = default('/configurations/hadoop-site/hdfs_user_keytab',None)
+
+spark_conf_dir = config['configurations']['spark']['spark_conf_dir']
---
+++ KEEDIO/1.2/services/SPARK/package/scripts/spark.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+from resource_management import *
+import sys
+
+from functools import partial
+from ambari_agent.AgentException import AgentException
+from subprocess import *
+from utils import execute_sudo_krb
+
+def spark(service=None,action=None):
+  if action == "config":
+    import params
+    
+    execute_hdfs = partial(execute_sudo_krb,user=params.hdfs_user,principal=params.hdfs_principal_name,keytab=params.hdfs_user_keytab)
+    execute_spark = partial(execute_sudo_krb,user=params.spark_user,principal=params.spark_principal,keytab=params.spark_keytab)
+
+    create_home = ["hdfs","dfs","-mkdir","-p",params.spark_home]
+    chown_home = ["hdfs","dfs","-chown",params.spark_user, params.spark_home]
+
+    upload_jars = ["hdfs","dfs","-put",params.spark_yarn_lib_dir,params.spark_home]
+ 
+    execute_hdfs(create_home)
+    execute_hdfs(chown_home)
+    execute_spark(upload_jars)
+
+    configurations = params.config['configurations']['spark-site']
+  
+    File(format("{spark_conf_dir}/spark-defaults.conf"),
+       content=Template("spark_defaults.j2"),
+       owner=params.spark_user,
+       group=params.spark_group
+    )
+
