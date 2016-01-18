ebd633f200ff1163b988fc51296008802f027a29
Juan Carlos Fernandez <jcfernandez@keedio.com>
2016-01-18 16:21:48 +0100

    KSTCK-16: Added package python-kazoo required for some zookeeper checks

--- KEEDIO/1.4/services/NAGIOS/package/scripts/exclude_nagios.py
+++ KEEDIO/1.4/services/NAGIOS/package/scripts/exclude_nagios.py
@@ -1,1 +1,1 @@
-exclude_packages=['nrpe','nagios-plugins','nagios-plugins-all','nagios-hadoop','nagiosplugin','nagios-plugins-nrpe']
+exclude_packages=['nrpe','nagios-plugins','nagios-plugins-all','nagios-hadoop','nagiosplugin','nagios-plugins-nrpe','python-kazoo']
