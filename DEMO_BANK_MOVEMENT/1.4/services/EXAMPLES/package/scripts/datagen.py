874ea57bf89a48df8f25262a0bcd8b71109148b9
root <root@master.sl3zquhxampuhf5fvd2upcbm1h.fx.internal.cloudapp.net>
2016-10-19 16:17:05 +0000

    Fixes to DEMO

---
+++ DEMO_BANK_MOVEMENT/1.4/services/EXAMPLES/package/scripts/datagen.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+import sys
+from subprocess import *
+from resource_management import *
+
+def datagen(action):
+  if action == "config":
+   import params
+    
+
+  if action == "install":
+    Logger.info("Installing Syslog datagenerator") 
+
+  if action == "start" or action == "stop" or action == "status":
+    cmd=Popen(['service','datagen',action],stdout=PIPE,stderr=PIPE)
+    out,err=cmd.communicate()
+
+    if action == "start" or action == "stop":
+      Logger.info("Data generator: "+str(action))
+      Logger.info(out)
+      Logger.info(err)
+    if action == "status":
+      from functions import check_rc
+      rc=cmd.returncode
+      Logger.info("Data generator %s: %s" % (action, cmd.returncode == 0))
+   
+      check_rc(rc,stdout=out,stderr=err)
+
---
+++ DEMO_BANK_MOVEMENT/1.4/services/EXAMPLES/package/scripts/params.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+from resource_management.libraries.functions.version import format_hdp_stack_version, compare_versions
+from resource_management import *
+
+config = Script.get_config()
+
+security_enabled = config['configurations']['cluster-env']['security_enabled']
+kerberos_cache_file = default('/configurations/cluster-env/kerberos_cache_file','/tmp/ccache_keytab')
+kerberos_domain = config['configurations']['cluster-env']['kerberos_domain']
+
+smoke_user_keytab = default('/configurations/cluster-env/smokeuser_keytab',None)
+smoke_user = config['configurations']['cluster-env']['smokeuser']
+
+spark_user = config['configurations']['spark']['spark_user']
+spark_group = config['configurations']['spark']['spark_group']
+spark_hdfs_home = config['configurations']['spark']['spark_hdfs_home']
+spark_local_home = config['configurations']['spark']['spark_local_home']
+
+spark_principal = default('/configurations/spark/spark_principal',None)
+spark_keytab = default('/configurations/spark/spark_keytab',None)
+
+sparkhs_principal_name = default('/configurations/spark/dfs.spark.kerberos.principal',None)
+sparkhs_keytab = default('/configurations/spark/sparkhs_keytab',None)
+
+hdfs_user = config['configurations']['hadoop-env']['hdfs_user']
+hdfs_principal_name = default('/configurations/hadoop-env/hdfs_principal_name',None)
+hdfs_user_keytab = default('/configurations/hadoop-env/hdfs_user_keytab',None)
+
+spark_conf_dir = config['configurations']['spark']['spark_conf_dir']
+spark_assembly = config['configurations']['spark']['spark_assembly']
+spark_examples_jar = config['configurations']['spark']['spark_examples_jar']
+
+namenode_host = default("/clusterHostInfo/namenode_host", ["none"])
+namenode_host =str(namenode_host[0])
+
