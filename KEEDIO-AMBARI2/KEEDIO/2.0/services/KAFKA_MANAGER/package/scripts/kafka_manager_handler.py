0b606c2b28ce167f8bcc8a60008c446f279652b0
DaniSancas <dani@sancas.es>
2017-06-13 17:33:06 +0200

    Improved Kafka Manager to dynamically get ZK nodes conf

--- KEEDIO-AMBARI2/KEEDIO/2.0/services/KAFKA_MANAGER/package/scripts/kafka_manager_handler.py
+++ KEEDIO-AMBARI2/KEEDIO/2.0/services/KAFKA_MANAGER/package/scripts/kafka_manager_handler.py
@@ -5,13 +5,18 @@ from subprocess import *
 def kafka_manager(action):
     import utils
     # FIX: When status is checked params  shouldn't be called
-    if action == 'start' or action == 'stop':
-        executed = Popen(["service", "kafka-manager", action], stdout=PIPE, stderr=PIPE)
-        out, err = executed.communicate()
-        Logger.info("Kafka Manager service:")
-        Logger.info(action)
-        Logger.info(str(out))
-        Logger.info(str(err))
+    if action != 'status':
+        import params
+        if action == 'config':
+            File('/etc/kafka-manager/conf/application.conf',
+                 content=Template('application.conf.j2'))
+        elif action == 'start' or action == 'stop':
+            executed = Popen(["service", "kafka-manager", action], stdout=PIPE, stderr=PIPE)
+            out, err = executed.communicate()
+            Logger.info("Kafka Manager service:")
+            Logger.info(action)
+            Logger.info(str(out))
+            Logger.info(str(err))
     elif action == "status":
         executed = Popen(["service", "kafka-manager", action], stdout=PIPE, stderr=PIPE)
         out, err = executed.communicate()
