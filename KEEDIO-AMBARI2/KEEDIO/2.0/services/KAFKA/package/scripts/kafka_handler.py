8e2dba06ed70bd11219975b84c432eaab4e32810
root <acomisso@keedio.com>
2017-06-28 08:06:42 +0000

    Added AMS support to Kafka

--- KEEDIO-AMBARI2/KEEDIO/2.0/services/KAFKA/package/scripts/kafka_handler.py
+++ KEEDIO-AMBARI2/KEEDIO/2.0/services/KAFKA/package/scripts/kafka_handler.py
@@ -1,5 +1,6 @@
 from resource_management import *
 from subprocess import *
+import os
 
 def kafka(action):
   import utils
@@ -24,17 +25,34 @@ def kafka(action):
           value=str(params.kafka_topic_conf[param])+"\n"
         kafka_server_properties+=param+"="+value
 
+      if params.has_metric_collector:
+          kafka_server_properties+="kafka.metrics.reporters=org.apache.hadoop.metrics2.sink.kafka.KafkaTimelineMetricsReporter\n"
+          kafka_server_properties+="kafka.timeline.metrics.reporter.enabled=true\n"
+          kafka_server_properties+="kafka.timeline.metrics.hosts="+params.ams_collector_hosts+"\n"
+          kafka_server_properties+="kafka.timeline.metrics.port="+params.metric_collector_port+"\n"
+          kafka_server_properties+="kafka.timeline.metrics.protocol="+params.metric_collector_protocol+"\n"
+
       File('/etc/default/kafka-systemd.sh',
           content=StaticFile('kafka-systemd.profiles.d'))
       File('/etc/kafka/conf/server.properties',
         content=kafka_server_properties,
         owner='kafka',
         group='kafka')
-      #if params.has_ganglia_server:
-      #  File('/etc/jmxtrans/config/jmxtrans.config',
-      #    content=Template('jmxtrans.j2')
-      #  )
-#        File('/etc/jmxtrans/config/KafkaMetrics.json',
+      #Create symlinks in Kafka lib dir to include ambari-metrics jars
+      # Temporary until the links will be created in rpm
+      if os.path.exists("/usr/lib/ambari-metrics-kafka-sink") and params.has_metric_collector:
+         Link("/usr/lib/kafka/libs/ambari-metrics-kafka-sink.jar",
+            to="/usr/lib/ambari-metrics-kafka-sink/ambari-metrics-kafka-sink.jar",
+             )
+         Link("/usr/lib/kafka/libs/commons-collections-3.2.2.jar",
+            to="/usr/lib/ambari-metrics-kafka-sink/lib/commons-collections-3.2.2.jar",
+             )
+	 Link("/usr/lib/kafka/libs/commons-lang-2.6.jar",
+            to="/usr/lib/ambari-metrics-kafka-sink/lib/commons-lang-2.6.jar",
+             )
+         Link("/usr/lib/kafka/libs/commons-logging-1.1.1.jar",
+            to="/usr/lib/ambari-metrics-kafka-sink/lib/commons-logging-1.1.1.jar",
+             )
       if params.has_ganglia_server:
           File('/var/lib/jmxtrans/KafkaMetrics.json',
                content=Template('KafkaMetrics.json.j2')
