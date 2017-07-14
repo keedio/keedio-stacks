a12ed89118b654cdc5c58c148ae67b74d429a027
DaniSancas <dani@sancas.es>
2017-07-14 11:57:30 +0200

    Fix Nifi ZooKeeper connection string

--- KEEDIO-AMBARI2/KEEDIO/2.0/services/NIFI/package/scripts/params.py
+++ KEEDIO-AMBARI2/KEEDIO/2.0/services/NIFI/package/scripts/params.py
@@ -9,7 +9,7 @@ from resource_management.libraries.functions.default import default
     
 # server configurations
 config = Script.get_config()
-zookeeper_server_hosts = str(default("/configurations/kafka-server-properties/zookeeper.connect",None))
+zookeeper_server_hosts = str(default("/configurations/nifi-ambari-properties/zookeeper.connect",None))
 
 nifi_dirname = 'nifi'
 
