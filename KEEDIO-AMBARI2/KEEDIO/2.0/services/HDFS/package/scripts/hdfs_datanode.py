faf9f4e15c428224c1baae55c6701e85d2e68af0
root <acomisso@keedio.com>
2017-08-07 13:34:29 +0000

    HDFS: Added support for mounted_dirs_helper, now it realises if a datanode volume is unmounted

--- KEEDIO-AMBARI2/KEEDIO/2.0/services/HDFS/package/scripts/hdfs_datanode.py
+++ KEEDIO-AMBARI2/KEEDIO/2.0/services/HDFS/package/scripts/hdfs_datanode.py
@@ -18,26 +18,49 @@ limitations under the License.
 """
 
 from resource_management import *
-#from resource_management.libraries.functions.dfs_datanode_helper import handle_dfs_data_dir
+from resource_management.libraries.functions.mounted_dirs_helper import handle_mounted_dirs
 from utils import * 
 from subprocess import *
 import os
 
+def create_dirs(data_dir):
+  """
+  :param data_dir: The directory to create
+  :param params: parameters
+  """
+  import params
+  Directory(data_dir,
+            create_parents = True,
+            cd_access="a",
+            mode=0755,
+            owner=params.hdfs_user,
+            group=params.user_group,
+            ignore_failures=True
+  )
 def datanode(action=None):
 
   if action == "configure":
     import params
     # We do not need to set particular permission here, the datanode sets the right permission when it is started.
-    Directory(params.dfs_data_dir.split(','),
-        owner=params.hdfs_user,
-        group=params.user_group,
-        create_parents=True
-    )
+    #Directory(params.dfs_data_dir.split(','),
+    #    owner=params.hdfs_user,
+    #    group=params.user_group,
+    #    create_parents=True
+    #)
     Directory([params.dfs_domain_socket_dir],
         owner=params.hdfs_user,
         group=params.user_group,
         create_parents=True,
         mode=0751 )
+
+    data_dir_to_mount_file_content = handle_mounted_dirs(create_dirs, params.dfs_data_dirs, params.data_dir_mount_file, params)
+    # create a history file used by handle_mounted_dirs
+    File(params.data_dir_mount_file,
+         owner=params.hdfs_user,
+         group=params.user_group,
+         mode=0644,
+         content=data_dir_to_mount_file_content
+    )
 
   if action == "start":
     cmd=Popen(['service','hadoop-hdfs-datanode',action],stdout=PIPE,stderr=STDOUT)
